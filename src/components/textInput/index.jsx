74df73d0b898e23652bf3c9d9cfca848ce13c19d
dev-Beltramin <paulobeltramin.27@gmail.com>
2024-07-17 23:06:42 -0300

    criando funcionalidade de adicionar comentarios

--- src/components/textInput/index.jsx
+++ src/components/textInput/index.jsx
@@ -1,20 +1,31 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useState, useRef } from "react";
 import styles from "./textInput.module.scss";
 
 export const TextInput = ({ placeHolder, maxLength }) => {
   const [caracter, setCaracter] = useState("");
+  const [input, setInput] = useState("");
+  let InputValue = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const coment = InputValue.current.value;
+
+    setInput(coment);
+  };
 
   return (
     <main>
       <section className={styles.container}>
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>React--X </h1>
 
           <div>
             <textarea
               placeholder={placeHolder}
               maxLength={maxLength}
+              ref={InputValue}
               value={caracter}
               onChange={(e) => setCaracter(e.target.value)}
             />
@@ -23,13 +34,16 @@ export const TextInput = ({ placeHolder, maxLength }) => {
             {" "}
             {caracter.length}/ {maxLength}
           </span>
+          <div>
+            <button>Twwitar</button>
+          </div>
         </form>
       </section>
 
       <section className={styles.coments}>
         <img src="" alt="" />
 
-        <p>comentario</p>
+        <p>{input}</p>
       </section>
     </main>
   );
